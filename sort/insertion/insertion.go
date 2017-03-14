b18871f627c2a515bdd82831e8e918afeda4e5f1
“fudali113” <fudali113@gmail.com>
2017-03-14 18:51:08 +0800

    添加go的shell sort

--- sort/insertion/insertion.go
+++ sort/insertion/insertion.go
@@ -1,6 +1,7 @@
 package main
 
 func sort(array []int) {
+	n := 0
 	// 从1开始遍历,因为0前面没有数值
 	for i := 1; i < len(array); i++ {
 		// 在此处放置与在 代码1 处放置效果一样
@@ -10,10 +11,12 @@ func sort(array []int) {
 			// 代码1  iv := array[j]
 
 			// 如果 jv > iv , 调换两个元素之间的位置
+			n++
 			array[j], array[j-1] = array[j-1], iv
 
 		}
 	}
+	println("----------------", n)
 }
 
 func main() {
