36cd76a29fe5594dac798b67163e5a7da4535008
“fudali113” <fudali113@gmail.com>
2017-03-20 22:26:12 +0800

    完美

--- adt/head.go
+++ adt/head.go
@@ -5,7 +5,7 @@ import "fmt"
 // Head 堆结构
 // 数组形式为index为0的元素默认为空，总index为1元素开始
 // 方便定位元素
-// 即如忘堆里插入1,2,3,4,5
+// 即如往堆里插入1,2,3,4,5
 // 数组元素为[0,1,2,3,4,5]
 type Head struct {
 	array []int
@@ -13,43 +13,67 @@ type Head struct {
 }
 
 // GetHead 获取一个Head实列
-func GetHead(isMax bool) Head {
-	return Head{array: make([]int, 1, 8), isMax: isMax}
+func GetHead(isMax bool) *Head {
+	return &Head{array: make([]int, 1, 8), isMax: isMax}
 }
 
 // Insert 插入一个元素
-func (h Head) Insert(v int) {
+func (h *Head) Insert(value int) {
 	if h.array == nil {
 		h.array = make([]int, 1, 8)
 	}
-
+	h.array = insert(h.array, value, h.isMax)
 }
 
+// Delete 删除并获取堆顶
+func (h *Head) Delete() (top int, err error) {
+	h.array, top, err = reset(h.array, h.isMax)
+	return top, err
+}
+
+// getCompareResult 比较
+func getCompareResult(o1, o2 int, isMax bool) int {
+	if o1 == o2 {
+		return 0
+	}
+	if isMax {
+		if o1 > o2 {
+			return 1
+		}
+		return -1
+	}
+	if o1 < o2 {
+		return 1
+	}
+	return -1
+}
+
+//
+//
+// Insert 相关函数
+//
+//
+
 // insert 根据堆性质想数组中插入一个元素
-func insert(array []int, value int) (nowArray []int) {
+func insert(array []int, value int, isMax bool) (nowArray []int) {
 	nowArray = append(array, 0)
-	insertIndex := getInsertIndex(nowArray, len(array), value)
+	insertIndex := getInsertIndex(nowArray, len(array), value, isMax)
 	nowArray[insertIndex] = value
 	return nowArray
 }
 
 // getInsertIndex 递归获取插入元素的数组下标
 // 使用上虑策略
-func getInsertIndex(array []int, nowIndex int, value int) int {
+func getInsertIndex(array []int, nowIndex int, value int, isMax bool) int {
 	if nowIndex < 2 {
 		return 1
 	}
 	faIndex := getFaIndex(nowIndex)
-	if array[faIndex] >= value {
+	if getCompareResult(array[faIndex], value, isMax) >= 0 {
 		return nowIndex
 	}
 	array[faIndex], array[nowIndex] = array[nowIndex], array[faIndex]
-	return getInsertIndex(array, faIndex, value)
-}
-
-// getSonIndex 获取一个元素的左右子元素数组下标
-func getSonIndex(index int) (left, right int) {
-	return 2 * index, 2*index + 1
+	return getInsertIndex(array, faIndex, value, isMax)
 }
 
 // getFaIndex 获取一个元素的父级元素数组下标
@@ -57,49 +81,11 @@ func getFaIndex(index int) int {
 	return index / 2
 }
 
-// Delete 删除并获取堆顶
-func (h Head) Delete() (int, error) {
-	if len(h.array) < 2 {
-		return 0, fmt.Errorf("改堆内以没有元素")
-	}
-	top := h.array[1]
-	h.array = reset(h.array)
-	return top, nil
-}
-
-// reset 重设置一个数组
-func reset(array []int) []int {
-	array[1] = 0
-	value := array[len(array)-1]
-	nowArray := array[:len(array)-1]
-	lastInsertIndex := getLastInsertIndex(nowArray, 1, value)
-	nowArray[lastInsertIndex] = value
-	return nowArray
-}
-
-// getLastInsertIndex 下虑
-func getLastInsertIndex(array []int, nowIndex int, value int) int {
-	status := noGo
-	lastIndex := len(array) - 1
-	leftIndex, rightIndex := getSonIndex(nowIndex)
-	if lastIndex >= rightIndex {
-		left, right := array[leftIndex], array[rightIndex]
-		status = getStatus(left, right, value)
-	} else if lastIndex == leftIndex && array[leftIndex] > value {
-		status = goLeft
-	}
-	switch status {
-	case noGo:
-		return nowIndex
-	case goLeft:
-		array[nowIndex], array[leftIndex] = array[leftIndex], value
-		return getLastInsertIndex(array, leftIndex, value)
-	case goRight:
-		array[nowIndex], array[rightIndex] = array[rightIndex], value
-		return getLastInsertIndex(array, rightIndex, value)
-	}
-	panic("--")
-}
+//
+//
+// Delete 相关函数
+//
+//
 
 const (
 	noGo = iota
@@ -107,15 +93,62 @@ const (
 	goRight
 )
 
+// reset 重设置一个数组
+func reset(array []int, isMax bool) (nowArray []int, topValue int, err error) {
+	lastIndex := len(array) - 1
+	if lastIndex == 1 {
+		topValue = array[1]
+		nowArray = array[:lastIndex]
+		return
+	} else if lastIndex < 1 {
+		return array, 0, fmt.Errorf("堆中已无元素")
+	}
+	topValue, array[1] = array[1], 0
+	value := array[len(array)-1]
+	nowArray = array[:len(array)-1]
+	lastInsertIndex := getLastInsertIndex(nowArray, 1, value, isMax)
+	nowArray[lastInsertIndex] = value
+	return
+}
+
+// getLastInsertIndex 下虑
+func getLastInsertIndex(array []int, nowIndex int, value int, isMax bool) int {
+	status := noGo
+	lastIndex := len(array) - 1
+	leftIndex, rightIndex := getSonIndex(nowIndex)
+	if lastIndex >= rightIndex {
+		left, right := array[leftIndex], array[rightIndex]
+		status = getStatus(left, right, value, isMax)
+	} else if lastIndex == leftIndex && getCompareResult(array[leftIndex], value, isMax) >= 0 {
+		status = goLeft
+	}
+	switch status {
+	case noGo:
+		return nowIndex
+	case goLeft:
+		array[nowIndex], array[leftIndex] = array[leftIndex], value
+		return getLastInsertIndex(array, leftIndex, value, isMax)
+	case goRight:
+		array[nowIndex], array[rightIndex] = array[rightIndex], value
+		return getLastInsertIndex(array, rightIndex, value, isMax)
+	}
+	panic("--")
+}
+
+// getSonIndex 获取一个元素的左右子元素数组下标
+func getSonIndex(index int) (left, right int) {
+	return 2 * index, 2*index + 1
+}
+
 // getStatus 根据三个值获取走那边获取步鄹
-func getStatus(left, right, value int) (res int) {
+func getStatus(left, right, value int, isMax bool) (res int) {
 	res = 0
-	if left > value || right > value {
+	if getCompareResult(left, value, isMax) > 0 || getCompareResult(right, value, isMax) > 0 {
 		res++
 	} else {
 		return
 	}
-	if right > left {
+	if getCompareResult(right, left, isMax) > 0 {
 		res++
 	}
 	return
--- adt/head_test.go
+++ adt/head_test.go
@@ -7,12 +7,12 @@ import (
 
 func Test_getInsertIndex(t *testing.T) {
 	test := []int{0, 1}
-	testInsertIndex := getInsertIndex(test, 1, 1)
+	testInsertIndex := getInsertIndex(test, 1, 1, true)
 	if testInsertIndex != 1 {
 		t.Error("getInsertIndex has bug")
 	}
 	test = append(test, 2)
-	testInsertIndex = getInsertIndex(test, 2, 2)
+	testInsertIndex = getInsertIndex(test, 2, 2, true)
 	if testInsertIndex != 1 {
 		t.Error("getInsertIndex has bug", testInsertIndex)
 	}
@@ -21,22 +21,52 @@ func Test_getInsertIndex(t *testing.T) {
 func Test_insert(t *testing.T) {
 	log.Println("--------------------")
 	test := []int{0}
-	test = insert(test, 1)
+	test = insert(test, 1, true)
 	log.Println(test)
-	test = insert(test, 5)
+	test = insert(test, 5, true)
 	log.Println(test)
-	test = insert(test, 3)
+	test = insert(test, 3, true)
 	log.Println(test)
-	test = insert(test, 4)
+	test = insert(test, 4, true)
 	log.Println(test)
 }
 
 func Test_reset(t *testing.T) {
 	test := []int{0, 5, 4, 3, 1}
-	test = reset(test)
+	test, _, _ = reset(test, true)
 	log.Println(test)
-	test = reset(test)
+	test, _, _ = reset(test, true)
 	log.Println(test)
-	test = reset(test)
+	test, _, _ = reset(test, true)
 	log.Println(test)
+}
+
+func Test_HeadInsert(t *testing.T) {
+	h := GetHead(true)
+	h.Insert(1)
+	h.Insert(2)
+	h.Insert(5)
+	h.Insert(4)
+	log.Println("max heap", h)
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+
+	h = GetHead(false)
+	h.Insert(1)
+	h.Insert(2)
+	h.Insert(5)
+	h.Insert(4)
+	log.Println("min heap", h)
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
+	log.Println(h.Delete())
 }
